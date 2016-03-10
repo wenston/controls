49b4b804a681ead2677fe7ae588214db5505198f
ByWhy <857996735@qq.com>
2016-03-10 14:49:00 +0800

    增加了复制

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,1 +1,16 @@
 require('../css/index.scss');
+require('./jquery.zclip.js');
+var swf = require('./ZeroClipboard.swf');
+$(function() {
+	var clip = $('.zclip');
+	$('.zclip').zclip({
+		path:'../../'+swf,
+		copy: function() { //复制内容   
+			return $('#mytext').html();
+		},
+		afterCopy: function() { //复制成功   
+			alert("");
+			/*$("<span id='msg'/>").insertAfter($('#copy_input')).text('复制成功');*/
+		}
+	});
+});
