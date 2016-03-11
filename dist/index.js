e413cd78b0f2b1be6c5a9ee26eb63fe1c598960d
ByWhy <857996735@qq.com>
2016-03-11 12:37:03 +0800

    增加了按钮

--- dist/index.js
+++ dist/index.js
@@ -52,12 +52,14 @@
 		$('.zero-clipboard').zclip({
 			path:'../../'+swf,
 			copy: function() { //复制内容   
-				return $('.language-html').text();
+				var _this=$(this);
+				return $(this).siblings().find('.language-html').text();
 			},
 			afterCopy: function() { //复制成功   
-				$('.zero-clipboard').find("span").css("color","#08a7ec").text("复制成功!");
+				var _this=$(this);
+				_this.find("span").css("color","#08a7ec").text("复制成功!");
 				setTimeout(function(){
-					$('.zero-clipboard').find("span").css("color","inherit").text("复制");
+					_this.find("span").css("color","inherit").text("复制");
 				},1000);
 			}
 		});
